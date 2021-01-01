f544b29a36a98b542e29468173c8325f5e2e856d
Sajal Mishra <sajalmishra44@gmail.com>
2021-01-01 22:01:24 +0530

    test

---
+++ src/test/java/first/sajal/AppTest.java
@@ -0,0 +1,27 @@
+package first.sajal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testSum(){
+        App app = new App();
+        assertEquals(5, app.sum(2, 3));
+    }
+}
